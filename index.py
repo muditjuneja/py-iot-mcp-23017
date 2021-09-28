adbce07e8294631c3aa5cfc93caff7272c08aa24
Mudit <muditjuneja@outlook.com>
2021-09-28 08:36:36 -0900

    More changes

--- index.py
+++ index.py
@@ -5,8 +5,14 @@ import busio
 import digitalio
 from adafruit_mcp230xx.mcp23017 import MCP23017
 i2c = busio.I2C(board.SCL, board.SDA)
-mcp = MCP23017(i2c) 
 
+mcps = []
+for item in i2c.scan():
+    print(item)
+    mcp = MCP23017(i2c,address=item) 
+    mcps.append(mcp)
+print(mcps)
+print(i2c.scan())
 
 app = Flask(__name__)
 
@@ -19,11 +25,14 @@ def hello():
     pin0.switch_to_output(value=False)
     return 'Hello, World!'
 
-@app.route('/toggle-state/<int:pin>/<int:state>')
-def toggleState(pin,state):
-    pin = mcp.get_pin(pin)
-    pin.switch_to_output(value=state)
-    return 'Pin ' + str(pin) + ' set to : ' + str(state)
+@app.route('/toggle-state/<int:icNumber>/<int:pin>/<int:state>')
+def toggleState(icNumber,pin,state):
+    mcp = mcps[icNumber]
+    print(mcp)
+    if mcp:
+        _pin = mcp.get_pin(pin)
+        _pin.switch_to_output(value=state)
+        return 'Pin ' + str(pin) + ' set to : ' + str(state)
 
 if __name__ =='__main__':  
     app.run(debug = True,host="0.0.0.0")  
